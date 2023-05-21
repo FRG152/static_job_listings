49030786259b17025f413e0a6c146fa1f49b042a
Flavio Roman Goncalez <romanflavio2@gmail.com>
2023-05-20 20:28:24 -0400

    changing the images

--- src/components/Info/Logo.jsx
+++ src/components/Info/Logo.jsx
@@ -1,7 +1,9 @@
+import img_01 from "../../../public/images/photosnap.svg";
+
 const Logo = ({ img }) => {
   return (
     <figure className="absolute left-[3%] -top-[12%] sm:static">
-      <img className="w-[60px] sm:w-auto" src={img} alt="img" />
+      <img className="w-[60px] sm:w-auto" src={img_01} alt="img" />
     </figure>
   );
 };
